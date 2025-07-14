4b759df3d357d11cc57507cf690e80e1baf063e3
nurfadillahsari <105841110422@student.unismuh.ac.id>
2025-07-14 17:56:35 +0800

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font"
+import * as SpalsScreen from "expo-splash-screen"; 
+import { useEffect } from "react";
+
+SpalsScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+  const [ loaded, Error ] = useFonts({
+    "Outfit-VariableFont" :
+    require ("../assets/fonts/Outfit-VariableFont_wght.ttf"),
+  })
+
+  useEffect(() => {
+    if(loaded && Error == null) {
+      SpalsScreen.hideAsync();
+    }
+  }, [loaded, Error])
+
+  if(!loaded && !Error) {
+    return null
+  }   
   return <Stack />;
 }
